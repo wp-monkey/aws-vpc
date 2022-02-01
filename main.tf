836accb7858ac9543172b616a31084aa9a64bb69
danfedick <daniel.fedick@hashicorp.com>
2022-02-01 11:09:36 -0500

    no locals and bringing forward pub and priv network lists into main.

--- main.tf
+++ main.tf
@@ -1,16 +1,12 @@
-locals {
-  cidr_block = "${var.cidr_prefix}.0.0/16"
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = var.name
-  cidr = local.cidr_block
+  cidr = "${var.cidr_prefix}.0.0/16"
 
   azs             = ["${var.region}a", "${var.region}b"]
-  private_subnets = var.private_sub_cidrs
-  public_subnets  = var.public_sub_cidrs
+  private_subnets = ["${var.cidr_prefix}.101.0/24", "${var.cidr_prefix}.102.0/24"]
+  public_subnets  = ["${var.cidr_prefix}.1.0/24", "${var.cidr_prefix}.2.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-locals {
-  cidr_prefix = "10.27"
-  cidr_block  = "${var.cidr_prefix}.0.0/16"
-}
-
 variable "region" {
   type        = string
   default     = "us-east-2"
@@ -16,15 +11,6 @@ variable "name" {
 }
 
 variable "cidr_prefix" {
-  default     = local.cidr_prefix
+  default     = 10.27
   description = "I chose 10.27 because it's a private subnet and because .27 is super random."
 }
-
-variable "public_sub_cidrs" {
-  default = ["${local.cidr_prefix}.101.0/24", "${local.cidr_prefix}.102.0/24"]
-}
-
-variable "private_sub_cidrs" {
-  default = ["${local.cidr_prefix}.1.0/24", "${local.cidr_prefix}.2.0/24"]
-}
-
