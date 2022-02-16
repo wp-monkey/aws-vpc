abc9284971d85be93a801974f5bbd193de0fefeb
danfedick <daniel.fedick@hashicorp.com>
2022-02-16 13:10:45 -0500

    Add public subnets

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,11 @@ output "vpc_id" {
 output "default_route_table_id" {
   value = module.vpc.vpc_main_route_table_id
 }
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
